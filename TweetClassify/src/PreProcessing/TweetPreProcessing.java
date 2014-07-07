f64da761e423b79e5f0eb7cba1dda87c18fcaad4
yxun <xuyuanlin_00@hotmail.com>
2014-07-06 19:08:31 -0700

    tweet tokenized and remove hashtags, @username

--- TweetClassify/src/PreProcessing/TweetPreProcessing.java
+++ TweetClassify/src/PreProcessing/TweetPreProcessing.java
@@ -7,6 +7,9 @@ import java.nio.charset.Charset;
 import java.nio.charset.MalformedInputException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class TweetPreProcessing {
 	// file is encoded in ASCII
@@ -15,6 +18,18 @@ public class TweetPreProcessing {
 	private static final String input = "test02";
 	// path of the output file
 	private static final String output = "test03";
+	
+	public static void hashtagApproach(String hashtag){
+		// classify tweets based on its hashtags
+		
+	}
+	
+	public static void socialApproach(String hashtag){
+		// classify tweets based on its @username
+		
+	}
+	
+	
 	
 	public static void main(String[] args){
 		
@@ -40,16 +55,43 @@ public class TweetPreProcessing {
 			
 		}*/
 		
-		// extract hashtags
+		// read input text file and 
+		
 		try{
+			// read input text file
 			BufferedReader reader = Files.newBufferedReader(Paths.get(input), charset);
+			// write output text file
 			BufferedWriter writer = Files.newBufferedWriter(Paths.get(output), charset);
+			// input line holder
 			String line = null;
 		
 			while ((line = reader.readLine()) != null){
-				
+				// tokenize input line
+				List<String> tokens = Arrays.asList(line.split(" "));
+				ArrayList<String> tokensArrayList = new ArrayList<String>();
+				tokensArrayList.addAll(tokens);
+				// extract hashtags, social relationships(@username)
+				for (int i = 0; i < tokensArrayList.size(); i++){
+					String s = tokensArrayList.get(i);
+					// extract hashtags
+					if (s.startsWith("#")){
+						hashtagApproach(s);
+						tokensArrayList.remove(i);
+					}
+					// extract social relationships
+					else if (s.startsWith("RT") || s.contains("@")){
+						socialApproach(s);
+						tokensArrayList.remove(i);
+					}
+					else {
+					writer.write(s+" ");
+					}
 			
+				}
+				writer.newLine();
+				
 			}
+			
 			writer.close();
 		}
 		catch(IOException e){
